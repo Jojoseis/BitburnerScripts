9890f027b96648b1f2ac7120874c49a4b0e32e3b
Jojoseis <70569002+Jojoseis@users.noreply.github.com>
2026-05-12 13:05:13 +0200

    add home server file distribution

--- config.mjs
+++ config.mjs
@@ -10,6 +10,9 @@ const createContext = async () =>
 				port: 12525,
 				types: "NetscriptDefinitions.d.ts",
 				pushOnConnect: true,
+				distribute: {
+					"servers/home": ["home"]
+				}
 			}),
 		],
 		bundle: false,
